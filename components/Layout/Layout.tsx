2f0886d67a76327c07be19087830ac9a94e026b1
Matt Claffey <mattclaffey93@gmail.com>
2020-01-15 20:38:51 +0000

    feat: :sparkles: add titan-ui & jest

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,6 +1,5 @@
-import * as React from 'react'
-import Link from 'next/link'
-import Head from 'next/head'
+import * as React from 'react';
+import Head from 'next/head';
 
 type Props = {
   title?: string
@@ -10,25 +9,14 @@ const Layout: React.FunctionComponent<Props> = ({
   children,
   title = 'This is the default title',
 }) => (
-  <div>
+  <>
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-      </nav>
-    </header>
     {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
-  </div>
+  </>
 )
 
 export default Layout
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,36 @@
-import * as React from 'react'
-import Layout from '../components/Layout'
-import { NextPage } from 'next'
+import * as React from "react";
+import Layout from "../components/Layout/Layout";
+import { NextPage } from "next";
+
+// Todo: Titan Tooling needs type definintions added to the npm module.
+// @ts-ignore
+import { Region, RegionInner, Grid, GridItem } from "@titan-tooling/ui";
+
+import "@titan-tooling/ui/dist/css/main.css";
+import "../styles/index.css";
+import { Intro } from "../components/Intro";
 
 const IndexPage: NextPage = () => {
   return (
-    <Layout title="Home | Next.js + TypeScript Example">
-      <h1>Hello Next.js 👋</h1>
+    <Layout title="Compare your Air">
+      <Region
+        style={{
+          "--region-text-color": "var(--white)"
+        }}
+      >
+        <RegionInner additionalClassNames="o-region--full-height o-region--center-screen">
+          <Grid>
+            <GridItem columns={{ xs: '12' }}>
+              <Intro title="Composer of Air">
+                Compare air quality between cities in the UK. <br />
+                Select cities to compare using the search tool below. 
+              </Intro>
+            </GridItem>
+          </Grid>
+        </RegionInner>
+      </Region>
     </Layout>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
