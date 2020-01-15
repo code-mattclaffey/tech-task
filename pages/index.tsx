18f6adf4fbae6f16a9e9bee3a571348cb204df1f
Matt Claffey <mattclaffey93@gmail.com>
2020-01-15 17:30:44 +0000

    chore: :hammer: move all unwanted stuff out of the codebase

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,11 +7,6 @@ const IndexPage: NextPage = () => {
   return (
     <Layout title="Home | Next.js + TypeScript Example">
       <h1>Hello Next.js 👋</h1>
-      <p>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-      </p>
     </Layout>
   )
 }
