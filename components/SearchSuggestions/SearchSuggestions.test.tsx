c0c679d0bda1539f60bd51d0940bc1e276831364
Matt Claffey <mattclaffey93@gmail.com>
2020-01-18 14:39:04 +0000

    fix: :bug: remove app from searchSuggestions

--- components/SearchSuggestions/SearchSuggestions.test.tsx
+++ components/SearchSuggestions/SearchSuggestions.test.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { render, cleanup, fireEvent } from "@testing-library/react";
 import { SearchSuggestions } from ".";
-import { App } from "../App";
 import { SearchContextProvider } from '../SearchContext';
 
 afterEach(() => {
